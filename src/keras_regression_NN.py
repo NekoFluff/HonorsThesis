4a57bd4d8b9972e0c8cc8ad395dbe495a3c73461
Alex <alexnou@gmail.com>
2018-12-09 15:56:57 -0700

    Retrieved embeddings for static environment

--- src/keras_regression_NN.py
+++ src/keras_regression_NN.py
@@ -5,8 +5,38 @@ from tensorflow import keras
 
 import numpy as np
 
-print(tf.version)
+print(tf.__version__)
 
 ##############################################################
 #  Get Boston Housing Prives dataset
 ##############################################################
+boston_housing = keras.datasets.boston_housing
+
+(train_data, train_labels), (test_data, test_labels) = boston_housing.load_data()
+
+# Shuffle the training set
+order = np.argsort(np.random.random(train_labels.shape))
+train_data = train_data[order]
+train_labels = train_labels[order]
+
+print("Training set: {}".format(train_data.shape))  # 404 examples, 13 features
+print("Testing set:  {}".format(test_data.shape))   # 102 examples, 13 features
+
+import pandas as pd
+
+column_names = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD',
+                'TAX', 'PTRATIO', 'B', 'LSTAT']
+
+df = pd.DataFrame(train_data, columns=column_names)
+print(df.head())
+
+print(train_labels[0:10])  # Display first 10 entries
+
+# Test data is *not* used when calculating the mean and std
+
+mean = train_data.mean(axis=0)
+std = train_data.std(axis=0)
+train_data = (train_data - mean) / std
+test_data = (test_data - mean) / std
+
+print(train_data[0])  # First training sample, normalized
--- src/user_item_recommendation.py
+++ src/user_item_recommendation.py
@@ -122,7 +122,7 @@ partial_y_train = train_labels[testing_samples:]
 
 history = model.fit(partial_x_train,
                     partial_y_train,
-                    epochs=15,
+                    epochs=20,
                     batch_size=512,
                     validation_data=(x_val, y_val),
                     verbose=1)
